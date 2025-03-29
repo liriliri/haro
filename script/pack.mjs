34c3bc65ca36c665fbf7c64a66506c5014bdef07
redhoodsu <redhoodsu@tencent.com>
2025-03-29 19:47:06 +0800

    feat: icon

---
+++ script/pack.mjs
@@ -0,0 +1,31 @@
+import builder from 'electron-builder'
+
+cd('dist')
+
+const pkg = await fs.readJson('package.json')
+
+const config = {
+  directories: {
+    output: `../release/${pkg.version}`,
+  },
+  files: ['main', 'preload', 'renderer'],
+  artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
+  nsis: {
+    allowToChangeInstallationDirectory: true,
+    oneClick: false,
+  },
+  win: {
+    target: [
+      {
+        target: 'nsis',
+      },
+    ],
+  },
+  mac: {
+    electronLanguages: ['zh_CN', 'en'],
+  },
+}
+
+await builder.build({
+  config,
+})
