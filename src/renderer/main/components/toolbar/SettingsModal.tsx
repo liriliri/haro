56f2c6245246e9dd9bf83d3c69ae993462536c21
redhoodsu <redhoodsu@tencent.com>
2025-06-13 12:33:19 +0800

    chore: small changes

--- src/renderer/main/components/toolbar/SettingsModal.tsx
+++ src/renderer/main/components/toolbar/SettingsModal.tsx
@@ -7,7 +7,7 @@ import LunaSetting, {
   LunaSettingTitle,
 } from 'luna-setting/react'
 import { t } from '../../../../common/util'
-import Style from './Settings.module.scss'
+import Style from './SettingsModal.module.scss'
 import { createPortal } from 'react-dom'
 import { observer } from 'mobx-react-lite'
 import store from '../../store'
